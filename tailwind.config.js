5b0b592cfffe22cfc9f8d75ddf39b84913bd0536
Daizy <daizyhasija.14@gmail.com>
2021-08-06 00:24:15 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  important: true,
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ["group-hover"],
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
