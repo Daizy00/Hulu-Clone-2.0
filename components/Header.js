168f06d07a8ef6dc429519f162253c198e68f4f8
Daizy <daizyhasija.14@gmail.com>
2021-08-06 14:09:39 +0530

    added more responsiveness

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,8 @@ import {
 
 function Header() {
   return (
-    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-      <div className="flex flex-grow justify-evenly max-w-2xl">
+    <header className="flex flex-col-reverse m-5 sm:flex-row justify-between items-center h-auto">
+      <div className="flex flex-grow justify-between max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title=" TRENDING" Icon={LightningBoltIcon} />
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -2,9 +2,9 @@ function HeaderItem({ title, Icon }) {
   return (
     <div
       className="flex flex-col items-center cursor-pointer group
-          w-12 sm:w-20 hover:text-white"
+          w-12 sm:w-20 hover:text-white mx-1"
     >
-      <Icon className="h-8 mb-1 group-hover:animate-bounce " />
+      <Icon className="h-6 sm:h-8 my-1 group-hover:animate-bounce" />
       <p
         className="opacity-0 group-hover:opacity-100
                tracking-widest"
